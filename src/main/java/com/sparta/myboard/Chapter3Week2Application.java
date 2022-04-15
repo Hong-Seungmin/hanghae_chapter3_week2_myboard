36337fd7171432454d307aef5d28de628076d93d
Seungmin <globalsh1@gmail.com>
2022-04-15 15:28:30 +0900

    Initial commit

---
+++ src/main/java/com/sparta/myboard/Chapter3Week2Application.java
@@ -0,0 +1,13 @@
+package com.sparta.myboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter3Week2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter3Week2Application.class, args);
+    }
+
+}
