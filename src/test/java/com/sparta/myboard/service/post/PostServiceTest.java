57dcff4d40f2c1a470ca94cedd2e85dfdc560d34
Seungmin <globalsh1@gmail.com>
2022-04-23 15:24:55 +0900

    포스트 서비스 테스트 완료

--- src/test/java/com/sparta/myboard/service/post/PostServiceTest.java
+++ src/test/java/com/sparta/myboard/service/post/PostServiceTest.java
@@ -1,9 +1,33 @@
 package com.sparta.myboard.service.post;
 
-import com.sparta.myboard.BaseIntegrationTest;
+import com.sparta.myboard.repository.LikesRepository;
+import com.sparta.myboard.repository.PostRepository;
+import com.sparta.myboard.repository.UserRepository;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-public class PostServiceTest extends BaseIntegrationTest {
+@ExtendWith(MockitoExtension.class)
+public class PostServiceTest {
+
+    @InjectMocks
+    private PostService postService;
+
+    @Mock
+    private PostRepository postRepository;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private LikesRepository likesRepository;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     public void 포스트_저장() throws Exception {
