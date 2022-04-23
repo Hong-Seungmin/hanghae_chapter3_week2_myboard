00d6a1388676bc0cbd059587ee4e031021ad3fb0
Seungmin <globalsh1@gmail.com>
2022-04-24 01:10:47 +0900

    설정 상수로 표현, 변경

--- src/main/java/com/sparta/myboard/config/security/CorsConfig.java
+++ src/main/java/com/sparta/myboard/config/security/CorsConfig.java
@@ -2,6 +2,7 @@ package com.sparta.myboard.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -16,10 +17,11 @@ public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        config.addExposedHeader("Authorization");
+        config.addAllowedOrigin(CorsConfiguration.ALL);
+//        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedHeader(CorsConfiguration.ALL);
+        config.addAllowedMethod(CorsConfiguration.ALL);
+        config.addExposedHeader(HttpHeaders.AUTHORIZATION);
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
         }
