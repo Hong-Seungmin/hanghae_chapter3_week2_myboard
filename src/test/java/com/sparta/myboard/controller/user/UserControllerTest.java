880633c8c2640fccf6303e0758625f3aaf4ce455
Seungmin <globalsh1@gmail.com>
2022-04-23 21:57:37 +0900

    유저컨트롤 테스트 일부 완료/ 로그아웃 작업 필요함

--- src/test/java/com/sparta/myboard/controller/user/UserControllerTest.java
+++ src/test/java/com/sparta/myboard/controller/user/UserControllerTest.java
@@ -1,12 +1,16 @@
 package com.sparta.myboard.controller.user;
 
+import com.sparta.myboard.config.security.jwt.JwtTokenProvider;
 import com.sparta.myboard.controller.BaseIntegrationTest;
+import com.sparta.myboard.domain.user.UserLoginRequestDto;
 import com.sparta.myboard.domain.user.UserRegisterRequestDto;
+import com.sparta.myboard.repository.UserRepository;
 import com.sparta.myboard.service.user.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.web.servlet.ResultActions;
@@ -20,17 +24,24 @@ public class UserControllerTest extends BaseIntegrationTest {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
 
     @Autowired
     private WebApplicationContext context;
+
+
+    private String username = "username";
+    private String password = "password";
+    private String nickname = "nickname";
+
 
     @Nested
     @DisplayName("회원가입")
     @WithAnonymousUser
     public class register {
-        private String username = "username";
-        private String password = "password";
-        private String nickname = "nickname";
 
         @Test
         public void 성공() throws Exception {
@@ -116,96 +127,114 @@ public class UserControllerTest extends BaseIntegrationTest {
         @Test
         public void 성공() throws Exception {
             //given
+            UserLoginRequestDto requestDto = new UserLoginRequestDto(username, password);
+
+
+            userService.registerUser(username, nickname, password);
 
             //when
+            ResultActions resultActions = mvc.perform(post("/api/user/login")
+                                                              .contentType(MediaType.APPLICATION_JSON)
+                                                              .content(objectMapper.writeValueAsString(requestDto))
+                                                              .accept("*/*"));
+
 
             //then
-        }
+            resultActions.andExpect(status().isOk())
+                         .andExpect(jsonPath("$.success").value(true));
 
-        @Test
-        public void 성공_앞_뒤로_띄워쓰기_포함() throws Exception {
-            //given
-
-            //when
-
-            //then
         }
 
         @Test
         public void 실패_로그인_인증_후_시도() throws Exception {
             //given
+            UserLoginRequestDto requestDto = new UserLoginRequestDto(username, password);
+
+            String token = JwtTokenProvider.generateToken(username);
+            userService.registerUser(username, nickname, password);
 
             //when
+            ResultActions resultActions = mvc.perform(post("/api/user/login")
+                                                              .header(HttpHeaders.AUTHORIZATION, token)
+                                                              .contentType(MediaType.APPLICATION_JSON)
+                                                              .content(objectMapper.writeValueAsString(requestDto))
+                                                              .accept("*/*"));
+
 
             //then
+            resultActions.andExpect(status().isBadRequest())
+                         .andExpect(jsonPath("$.msg").value("이미 로그인 중.."));
+
         }
 
         @Test
         public void 실패_비밀번호_틀림() throws Exception {
             //given
+            UserLoginRequestDto requestDto = new UserLoginRequestDto(username, password);
+
+            userService.registerUser(username, nickname, "sisisi");
 
             //when
+            ResultActions resultActions = mvc.perform(post("/api/user/login")
+                                                              .contentType(MediaType.APPLICATION_JSON)
+                                                              .content(objectMapper.writeValueAsString(requestDto))
+                                                              .accept("*/*"));
+
 
             //then
+            resultActions.andExpect(status().isBadRequest())
+                         .andExpect(jsonPath("$.msg").value("로그인할 수 없는 비밀번호입니다."));
+
         }
 
         @Test
         public void 실패_아이디_없음() throws Exception {
-            //given
 
-            //when
-
-            //then
-        }
-
-        @Test
-        public void 실패_아이디_공란() throws Exception {
-            //given
-
-            //when
-
-            //then
-        }
-
-        @Test
-        public void 실패_비밀번호_공란() throws Exception {
-            //given
-
-            //when
-
-            //then
-        }
-
-        @Test
-        public void 실패_아이디_띄워쓰기_포함() throws Exception {
-            //given
-
-            //when
-
-            //then
         }
     }
 
+
+    //TODO 이건 한번 조사해봐야함 로그아웃 기능자체가 없음
     @Nested
     @DisplayName("로그아웃")
     class logout {
 
         @Test
         public void 성공_로그아웃_클릭() throws Exception {
-            //given
 
-            //when
+//            //given
+//            String token = JwtTokenProvider.generateToken(username);
+//
+//            userService.registerUser(username, nickname, password);
+//            //when
+//            ResultActions resultActions = mvc.perform(post("/api/user/logout")
+//                                                              .header(HttpHeaders.AUTHORIZATION, token)
+//                                                              .accept("*/*"));
+//
+//
+//            //then
+//            resultActions.andExpect(status().isOk())
+//                         .andExpect(jsonPath("$.msg").value("로그아웃 성공"));
+//            assertFalse(SecurityContextHolder.getContext().getAuthentication().isAuthenticated());
 
-            //then
         }
 
         @Test
         public void 실패_토큰인증_오류() throws Exception {
             //given
+//            String token = JwtTokenProvider.generateToken(username);
+            String token = "asdasdasdd";
 
             //when
+            ResultActions resultActions = mvc.perform(post("/api/user/logout")
+                                                              .header(HttpHeaders.AUTHORIZATION, token)
+                                                              .accept("*/*"));
+
 
             //then
+            resultActions.andExpect(status().isBadRequest())
+                         .andExpect(jsonPath("$.msg").value("이미 로그인 중.."));
+
         }
     }
 
